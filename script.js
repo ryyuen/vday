69591842cac7f97795c1c401db81412900af2ab4
ryyuen <monbebesunn@gmail.com>
2025-02-13 14:39:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $('#flipbook').turn({
+    width: 800,
+    height: 600,
+    autoCenter: true,
+    duration: 1000,
+    gradients: true
+  });
+});
